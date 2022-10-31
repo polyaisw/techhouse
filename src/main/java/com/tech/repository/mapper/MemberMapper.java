54482bd5510f13ed45973de57e2d76951ae3c472
Elephant153 <sko03kim4@naver.com>
2022-10-31 09:23:56 +0900

    회원탈퇴 기능 추가
    ㅈㄱㄴ

--- src/main/java/com/tech/repository/mapper/MemberMapper.java
+++ src/main/java/com/tech/repository/mapper/MemberMapper.java
@@ -10,10 +10,16 @@ public interface MemberMapper {
 	/* 아이디 중복 검사 */
 	public int idCheck(String memberId);
 	
+	/* 닉네임 중복 검사 */
+	public int nameCheck(String membername);
+	
 	/* 로그인 */
 	public UserVO memberLogin(UserVO vo);
 	
 	/* 마이페이지 정보수정 */
 	public void memberEdit(UserVO vo);
+	
+	/* 회원 탈퇴 */
+	public void memberDel(UserVO vo);
 	
 }
--- src/main/java/com/tech/service/interfaces/UserService.java
+++ src/main/java/com/tech/service/interfaces/UserService.java
@@ -8,12 +8,16 @@ public interface UserService {
 	public void memberJoin(UserVO vo) throws Exception;
 	/* 아이디 중복 검사 */
 	public int idCheck(String memberId) throws Exception;
+	/* 아이디 중복 검사 */
+	public int nameCheck(String membername) throws Exception;
 	/* 로그인 */
 	public UserVO memberLogin(UserVO vo) throws Exception; 
 	/* 마이페이지 본인확인 */
 	public UserVO mypageCK(UserVO vo) throws Exception; 
 	/* 마이페이지 정보수정 */
 	public void memberEdit(UserVO vo) throws Exception; 
+	/* 회원 탈퇴 */
+	public void memberDel(UserVO vo) throws Exception; 
 //	UserVO getUser(UserVO vo);		//유저정보
 //	void deleteUser(UserVO vo);	//회원탈퇴
 }
